01755d3fb40c2b8c6ddefca7779bd264bb69b28e
Eliquan <44224831+Eliquan@users.noreply.github.com>
2019-07-19 04:02:53 +0530

    placement of timer
    just placing for now, unity not accepting my scripts

--- massChaos/Assets/Timer.cs
+++ massChaos/Assets/Timer.cs
@@ -1,18 +1,85 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Timer : MonoBehaviour
 {
-    // Start is called before the first frame update
+    Image timeBar;
+    public float Daymax;
+    public float Nightmax;
+    public float dayTime;
+    public float nightTime;
+    public GameObject DayText;
+    public GameObject NightText;
+    public GameObject date;
+    float calen;
+    
+
+
+
+
     void Start()
     {
-        
+        DayText.SetActive(true);
+
+        timeBar = GetComponent<Image>();
+
+        dayTime = Daymax;
+
+        date = GameObject.Find("date");
+        calen = 1;
+
     }
 
-    // Update is called once per frame
+
+
     void Update()
     {
-        
+        if (dayTime >= 0)
+        {
+            
+
+            //calen = calen + h;
+
+            date.GetComponent<Text>().text = "Day" + calen.ToString();
+            nightTime = Nightmax;
+            NightText.SetActive(false);
+            dayTime -= Time.deltaTime;
+            timeBar.fillAmount = dayTime / Daymax;
+            if (timeBar.fillAmount == 0)
+            {
+                calen = calen + 1;
+            }
+            DayText.SetActive(true);
+            
+        }
+        else
+        {
+
+
+            NightText.SetActive(true);
+            DayText.SetActive(false);
+            //Time.timeScale = 0; 
+            nightTime -= Time.deltaTime;
+            timeBar.fillAmount = nightTime / Nightmax;
+
+            if (nightTime <= 0)
+            {
+                dayTime = Daymax;
+               
+            }
+            if (nightTime == 0)
+            { calen -= 1; }
+
+
+
+        }
+
+
+
     }
+
+
+
 }
