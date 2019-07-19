27d4c231ad63e3f31612c974555d2ae113c5f507
Eliquan <44224831+Eliquan@users.noreply.github.com>
2019-07-19 17:51:16 +0530

    day/night + days+ play/pause
    The day and roatation is set, accordingly the day count increases.
    The play and pause button inseretd pausing only the time right now have to make and exposed link for others to use.
    time going in update function, but wont start unless the play button is pressed.

--- massChaos/Assets/Timer.cs
+++ massChaos/Assets/Timer.cs
@@ -8,12 +8,17 @@ public class Timer : MonoBehaviour
     Image timeBar;
     public float Daymax;
     public float Nightmax;
-    public float dayTime;
-    public float nightTime;
+    public static float dayTime;
+    public static float nightTime;
     public GameObject DayText;
     public GameObject NightText;
     public GameObject date;
+    public GameObject tim;
     float calen;
+    public bool timerActive = false;
+    public Text timevalue;
+    public Text playButtonTxt;
+    //float n;
     
 
 
@@ -22,13 +27,16 @@ public class Timer : MonoBehaviour
     void Start()
     {
         DayText.SetActive(true);
+        NightText.SetActive(false);
 
         timeBar = GetComponent<Image>();
 
         dayTime = Daymax;
+        tim = GameObject.Find("time");
 
         date = GameObject.Find("date");
         calen = 1;
+        
 
     }
 
@@ -36,48 +44,59 @@ public class Timer : MonoBehaviour
 
     void Update()
     {
-        if (dayTime >= 0)
+        if (timerActive)
         {
-            
-
-            //calen = calen + h;
-
-            date.GetComponent<Text>().text = "Day" + calen.ToString();
-            nightTime = Nightmax;
-            NightText.SetActive(false);
-            dayTime -= Time.deltaTime;
-            timeBar.fillAmount = dayTime / Daymax;
-            if (timeBar.fillAmount == 0)
+            if (dayTime >= 0)
             {
-                calen = calen + 1;
+
+
+                //calen = calen + h;
+
+                date.GetComponent<Text>().text = "Day" + calen.ToString();
+                nightTime = Nightmax;
+                NightText.SetActive(false);
+                dayTime -= Time.deltaTime;
+                timeBar.fillAmount = dayTime / Daymax;
+                if (timeBar.fillAmount == 0)
+                {
+                    calen = calen + 1;
+                }
+                DayText.SetActive(true);
+               // timeBar.fillAmount = n;
+                timevalue.text = (timeBar.fillAmount * Daymax).ToString("F2");
+
             }
-            DayText.SetActive(true);
-            
-        }
-        else
-        {
-
-
-            NightText.SetActive(true);
-            DayText.SetActive(false);
-            //Time.timeScale = 0; 
-            nightTime -= Time.deltaTime;
-            timeBar.fillAmount = nightTime / Nightmax;
-
-            if (nightTime <= 0)
+            else
             {
-                dayTime = Daymax;
-               
+
+
+                NightText.SetActive(true);
+                DayText.SetActive(false);
+                //Time.timeScale = 0; 
+                nightTime -= Time.deltaTime;
+                timeBar.fillAmount = nightTime / Nightmax;
+                timevalue.text = (timeBar.fillAmount * Nightmax).ToString("F2");
+
+                if (nightTime <= 0)
+                {
+                    dayTime = Daymax;
+
+                }
+                if (nightTime == 0)
+                { calen -= 1; }
+
+
+
             }
-            if (nightTime == 0)
-            { calen -= 1; }
-
-
 
         }
 
-
-
+    }
+    
+    public void playButton()
+    {
+        timerActive = !timerActive;
+        playButtonTxt.text = timerActive ? "Pause" : "Play";
     }
 
 
