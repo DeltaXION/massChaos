9813e3f6d16e64c37f41a2f259e6556ba7cdecf5
Shreeya Achar <shreeya24@gmail.com>
2019-07-20 01:33:14 +0530

    Click and collect

---
+++ massChaos/Assets/Scripts/basebuild/collect_wood.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class collect_wood : MonoBehaviour
+{
+    public Text woods;
+
+    public int woodCount;
+    // Start is called before the first frame update
+
+
+    public void OnItemClicked()
+{
+        //if(Input.GetMouseButtonDown(0))
+        woodCount++;
+        // = woodCount + 1;
+        Debug.Log(woodCount);
+        woods = GameObject.Find("woodCount").GetComponent<Text>();
+        woods.text = woodCount.ToString();
+
+    }
+}
---
+++ massChaos/Assets/Scripts/basebuild/trees.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class trees : MonoBehaviour
+{
+    public float fadeTIme;
+    public bool displayInfo;
+    public GameObject forestUI;    
+    // Start is called before the first frame update
+    void Start()
+    {
+        displayInfo = false;
+        forestUI.SetActive(displayInfo);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+    }
+    private void OnMouseOver()
+    {//display wood collection UI
+        displayInfo = true;
+        //Debug.Log("Mouse");
+        FadeInfo();
+    }
+    private void OnMouseExit()
+    {
+        displayInfo = false;
+        FadeInfo();
+    }
+    void FadeInfo()
+    {
+            forestUI.SetActive (displayInfo);
+    }
+
+}
