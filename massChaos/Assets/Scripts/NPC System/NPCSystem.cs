781c0b0cde91ac0e5393bef10b9ba81da634a37e
Rujul <rujulshringarpure@gmail.com>
2019-07-19 19:21:48 +0530

    NPC Base Charachteristics
    Base attributes defined and UI added to populate list

---
+++ massChaos/Assets/Scripts/NPC System/NPCSystem.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCSystem : MonoBehaviour
+{
+
+
+    public static List<BaseCharachteristics> followers = new List<BaseCharachteristics>();
+    // Start is called before the first frame update
+
+
+void Start()
+    {
+        followers.Add(new BaseCharachteristics(1, "Broom", 20, 30,"N"));
+        followers.Add(new BaseCharachteristics(2, "Groom", 25, 36, "F"));
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
