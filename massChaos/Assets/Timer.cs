473e70b43b6c195726079064657697f11d4a4721
Amar <amar.ravi@outlook.com>
2019-07-21 13:26:10 +0530

    Time: day/night cycle+skip+play/pause+travelTime. (#16)
    The day and night cycle has been set.
    skip button to skip time in a fast pace for now, have toadjust speed.
    season till Autumn,have yet toadjust more.
    dungeon Button : will be replaced with the trigger for entering dungeon.
    have to move to dungeon next.

--- massChaos/Assets/Timer.cs
+++ massChaos/Assets/Timer.cs
@@ -14,29 +14,37 @@ public class Timer : MonoBehaviour
     public GameObject NightText;
     public GameObject date;
     public GameObject tim;
-    float calen;
+    public GameObject art;
+    public float travel;
+    public float startDay;
+    public float time = 10;
+    public float wholeDaySeconds;
+    public float travelSkip;
     public bool timerActive = false;
     public Text timevalue;
     public Text playButtonTxt;
-    //float n;
+    float button;
+    float calen;
+    public GameObject player;
+    float playerSpeed;
     
-
-
-
-
+   
     void Start()
     {
         DayText.SetActive(true);
         NightText.SetActive(false);
-
         timeBar = GetComponent<Image>();
-
         dayTime = Daymax;
         tim = GameObject.Find("time");
-
         date = GameObject.Find("date");
         calen = 1;
-        
+        // art = travelToDungeon.GetComponent<travelToDungeon>().checkDungeonEntry;
+        art = GameObject.Find("travel");//.GetComponent<travelToDungeon>().checkDungeonEntry;
+        //  player = GameObject.Find("BB_Player");
+        // playerSpeed = GetComponent<BB_BasicControls>();
+       playerSpeed = GetComponent<BB_BasicControls>().speed;
+
+
 
     }
 
@@ -44,46 +52,64 @@ public class Timer : MonoBehaviour
 
     void Update()
     {
+        
+        travel = art.GetComponent<travelToDungeon>().checkDungeonEntry;
+        if (travel == 1)
+        {
+            timerActive = false;
+            playButtonTxt.text = "Play";
+            Debug.Log("dungeon entered, time paused");
+            button = 1;
+            time += travelSkip;
+            date.GetComponent<Text>().text = "Day" + calen.ToString("F0");
+            playerSpeed = 0;
+               
+        }
+        if (travel == 2)
+        {
+            timerActive = true;
+            playButtonTxt.text = "Pause";
+            button = 0;
+        }
+
+
         if (timerActive)
         {
+            time += Time.deltaTime;
+            calen = time / wholeDaySeconds;
+            date.GetComponent<Text>().text = "Day " + calen.ToString("F0");
+            playButtonTxt.text = "Pause";
+
             if (dayTime >= 0)
             {
 
-
-                //calen = calen + h;
-
-                date.GetComponent<Text>().text = "Day" + calen.ToString();
                 nightTime = Nightmax;
                 NightText.SetActive(false);
                 dayTime -= Time.deltaTime;
                 timeBar.fillAmount = dayTime / Daymax;
-                if (timeBar.fillAmount == 0)
-                {
-                    calen = calen + 1;
-                }
+                /* if (timeBar.fillAmount == 0)
+                 {
+                     calen = calen + 1;
+                 }*/
                 DayText.SetActive(true);
-               // timeBar.fillAmount = n;
-                timevalue.text = (timeBar.fillAmount * Daymax).ToString("F2");
+                // timeBar.fillAmount = n;
+                timevalue.text = (timeBar.fillAmount * Daymax).ToString("F0");
 
             }
             else
             {
-
-
                 NightText.SetActive(true);
                 DayText.SetActive(false);
                 //Time.timeScale = 0; 
                 nightTime -= Time.deltaTime;
                 timeBar.fillAmount = nightTime / Nightmax;
-                timevalue.text = (timeBar.fillAmount * Nightmax).ToString("F2");
+                timevalue.text = (timeBar.fillAmount * Nightmax).ToString("F0");
 
                 if (nightTime <= 0)
                 {
                     dayTime = Daymax;
 
                 }
-                if (nightTime == 0)
-                { calen -= 1; }
 
 
 
@@ -92,13 +118,17 @@ public class Timer : MonoBehaviour
         }
 
     }
-    
+
     public void playButton()
     {
-        timerActive = !timerActive;
-        playButtonTxt.text = timerActive ? "Pause" : "Play";
+        //if (button == 0)
+       // {
+            timerActive = !timerActive;
+            playButtonTxt.text = timerActive ? "Pause" : "Play";
+       // }
+
     }
 
 
-
 }
+
