c19250f0efd2ee7872e9fb153e9a5e290faf975c
Amar <amar.ravi@outlook.com>
2019-07-21 13:17:55 +0530

    Merge 2 - Diplomacy + Loot + NPC(Rujul) (#15)
    * NPC Application Base Code
    
    Classes of factions,npc CRUD methods, started with npc applications
    
    * Revert "NPC Application Base Code"
    
    This reverts commit ec8fb575d1518fb1dd8c9b3c32cb669b97b5c44f.
    
    * Revert "Revert "NPC Application Base Code""
    
    This reverts commit dab4772b56a75f730e3abfd90b17cc0942b5731a.

---
+++ massChaos/Assets/NPCSystem/Scripts/Froots.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Froots 
+{
+    public static char affinity;
+    public static float prestige;
+    public static float happinessIndex;
+
+
+    public static char Affinity
+    {
+        get { return affinity; }
+        set { affinity = value; }
+    }
+
+    public static float Prestige
+    {
+        get { return prestige; }
+        set { prestige = value; }
+    }
+
+    public float HappinessIndex
+    {
+        get { return happinessIndex; }
+        set { happinessIndex = value; }
+    }
+}
