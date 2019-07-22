1c0e2ea11af1b05fce70d1bf6cbcbedb743bbf70
Amar <amar.ravi@outlook.com>
2019-07-22 22:30:41 +0530

    Functional Merge - EnemyAI + NPC (Neyo) + Time + Dungeon + Diplomacy + Combat (Vaibhav) + Farming (#24)
    * Farm UI
    
    UI with list integration
    
    * Other UI things
    
    Absolute basic functionality. No list yet
    
    * FarmingCanvas prefab
    
    Changed Farm Menu to Farming Canvas prefab
    
    * UI final touchups
    
    Merge everything bruh

---
+++ massChaos/Assets/farming/PlantList.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PlantList 
+{
+   public GameObject ShowButton;
+
+    public int plantId;
+    public string plantName;
+    public bool winter;
+    public int growDuration;
+    public int foodReward;
+
+    public PlantList(int id, string name, bool winterCondition, int duration, int reward)
+    {
+        this.plantId = id;
+        this.plantName = name;
+        this.winter = winterCondition;
+        this.growDuration = duration;
+        this.foodReward = reward;
+    }
+
+    public int plantID
+    {
+        get { return plantID; }
+        set { plantID = value; }
+    }
+
+    public string PlantName
+    {
+        get { return plantName; }
+        set { plantName = value; }
+    }
+
+    public bool Winter
+    {
+        get { return winter; }
+        set { winter = value; }
+    }
+
+    public int GrowDuration
+    {
+        get { return growDuration; }
+        set { growDuration = value; }
+    }
+
+    public int FoodReward
+    {
+        get { return foodReward; }
+        set { foodReward = value; }
+    }
+    
+}
