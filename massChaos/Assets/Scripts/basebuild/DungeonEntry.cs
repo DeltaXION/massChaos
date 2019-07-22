0603868c360f83e5bb9488dc37f0b08157a6936b
Shreeya Achar <shreeya24@gmail.com>
2019-07-22 09:36:46 +0530

    Dungeon Entry and Conditions for building house

---
+++ massChaos/Assets/Scripts/basebuild/DungeonEntry.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DungeonEntry : MonoBehaviour
+{
+    public GameObject DungeonUI;
+    // Start is called before the first frame update
+    public void OnMouseDown()
+    {
+
+            DungeonUI.SetActive(true);
+            Debug.Log("Dungeon");
+
+
+
+    }
+}
---
+++ massChaos/Assets/Scripts/basebuild/HouseDragHandler.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class HouseDragHandler : MonoBehaviour, IDragHandler, IEndDragHandler {
+
+    Vector3 startPosition;
+    public GameObject house;
+    private GameObject linehandler;
+    private Vector3 mousepos;
+    //float x;
+    //float y;
+    //float z;
+    //public GameObject houseImg;
+    public int woodCountAvl;
+    public int houseBuilt;
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        //    //startPosition =  GameObject.Find("house_img").transform.position;
+        startPosition = transform.parent.position;
+
+            //    x = GameObject.Find("house_img").transform.position.x;
+            //    y =  GameObject.Find("house_img").transform.position.y;
+            //    Debug.Log("Staaart" + x);
+            //    Debug.Log("Staaart" + y);
+            //    //clone = (GameObject)Instantiate(houseImg, startPosition, Quaternion.identity);
+
+
+    }
+    public void OnDrag(PointerEventData eventData)
+    {
+
+
+            //clone.GameObject.transform.position = Input.mousePosition;
+            transform.position = Input.mousePosition;
+      
+
+
+        
+
+    }
+ 
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        woodCountAvl = woodCountAvl - 5;
+        if (woodCountAvl >= 5)
+        {
+            //transform.position = Vector3.zero;
+            //Destroy (clone, 0.1f);
+
+            transform.localPosition = startPosition;
+            //Debug.Log("Ennnnd" + transform.position.x);
+            //Debug.Log("Ennnnnd" + transform.position.y);
+
+            mousepos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            linehandler = Instantiate(house, Camera.main.ScreenToWorldPoint(Input.mousePosition), Quaternion.identity) as GameObject;
+            linehandler.transform.SetAsLastSibling();
+            linehandler.transform.position = mousepos;
+            linehandler.SetActive(true);
+            houseBuilt++;
+            Debug.Log(houseBuilt);
+        }
+
+
+
+
+
+    }
+}
