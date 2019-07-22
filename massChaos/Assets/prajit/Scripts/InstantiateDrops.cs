2f265d9964a1fcd5f714e6857d995c90d9d9c092
Amar <amar.ravi@outlook.com>
2019-07-22 22:33:51 +0530

    Functional Merge - EnemyAI + NPC (Neyo) + Time + Dungeon + Diplomacy + Combat(Vaibhav) + Farming+ Loot (#25)
    * Dungeon resources UI working with functionality
    
    Dungeon resources and items UI working, made different items as placeholders. Most of the functionality is working
    
    * Loot UI updated
    
    Loot ui updated ,the rest is in the previous push

--- massChaos/Assets/prajit/Scripts/InstantiateDrops.cs
+++ massChaos/Assets/prajit/Scripts/InstantiateDrops.cs
@@ -1,73 +1,78 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
 
-public class InstantiateDrops : MonoBehaviour
-{
-    public GameObject dungeonGold;
-    public static int dungeonNumber = 1;
-    public GameObject dungeonAprefab;
-    public GameObject dungeonBprefab;
-    public GameObject dungeonCprefab;
-    public GameObject dungeonDprefab;
-
-    // Start is called before the first frame update
-    void Awake()
+    public class InstantiateDrops : MonoBehaviour
     {
+        public GameObject dungeonGold;
+        public static int dungeonNumber = 0;
+        public GameObject dungeonAprefab;
+        public GameObject dungeonBprefab;
+        public GameObject dungeonCprefab;
+        public GameObject dungeonDprefab;
 
-
-        dropGold(1, 1, 0, 0);
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
-
-    public void dropGold(int enemyType, int enemyLevel,float enemyLocationX,float enemyLocationY)                               //drops gold based on enemy type and its level(basically dungeon runs). you need to pass
-    {                                                                                                                           //enemy type, enemy level amd player location(x,y)
-        float n = 0;
-        for (int i = 0; i < enemyType; i++)
+        // Start is called before the first frame update
+        void Awake()
         {
-            for (int j = 0; j < enemyLevel; j++)
-            {
 
-                Instantiate(dungeonGold, new Vector2(enemyLocationX + n, enemyLocationY + n), Quaternion.identity);
-                n += 0.3f;
+
+            dropGold(1, 1, 0, 0);
+            dropGold(1, 1, 0, 0);
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        public void dropGold(int enemyType, int enemyLevel,float enemyLocationX,float enemyLocationY)                               //drops gold based on enemy type and its level(basically dungeon runs). you need to pass
+        {                                                                                                                           //enemy type, enemy level amd player location(x,y)
+            float n = 0;
+            for (int i = 0; i < enemyType; i++)
+            {
+                for (int j = 0; j < enemyLevel; j++)
+                {
+
+                    Instantiate(dungeonGold, new Vector2(enemyLocationX + n, enemyLocationY + n), Quaternion.identity);
+                    n += 0.3f;
+                }
+            }
+
+            if(dungeonNumber==0)
+            {
+                //dungeonAprefab.gameObject.SetActive(true);
+                //Instantiate(dungeonAprefab, new Vector2(enemyLocationX + n, enemyLocationY + n), Quaternion.identity);
+                GameObject A = dungeonAprefab.gameObject.GetComponent<TreasureChest>().CheckLootNearChest() as GameObject;
+                GameObject B = A.gameObject.GetComponent<TreasureChest>().CheckLootNearChest() as GameObject;
+                B.gameObject.GetComponent<TreasureChest>().DropLootNearChest(1);
+            }
+
+            if (dungeonNumber == 1)
+            {
+                //dungeonBprefab.gameObject.SetActive(true);
+                //Instantiate(dungeonBprefab, new Vector2(enemyLocationX + n, enemyLocationY + n), Quaternion.identity);
+                GameObject C = dungeonAprefab.gameObject.GetComponent<TreasureChest>().CheckLootNearChest() as GameObject;
+                GameObject D = C.gameObject.GetComponent<TreasureChest>().CheckLootNearChest() as GameObject;
+                D.gameObject.GetComponent<TreasureChest>().DropLootNearChest(1);
+            }
+            if (dungeonNumber == 2)
+            {
+                //dungeonCprefab.gameObject.SetActive(true);
+                //Instantiate(dungeonCprefab, nw Vector2(enemyLocationX + n, enemyLocationY + n), Quaternion.identity);
+                GameObject E = dungeonAprefab.gameObject.GetComponent<TreasureChest>().CheckLootNearChest() as GameObject;
+                GameObject F = E.gameObject.GetComponent<TreasureChest>().CheckLootNearChest() as GameObject;
+                F.gameObject.GetComponent<TreasureChest>().DropLootNearChest(1);
+            }
+            if (dungeonNumber == 3)
+            {
+                //dungeonDprefab.gameObject.SetActive(true);
+                // Instantiate(dungeonDprefab, new Vector2(enemyLocationX + n, enemyLocationY + n), Quaternion.identity);
+                GameObject G = dungeonAprefab.gameObject.GetComponent<TreasureChest>().CheckLootNearChest() as GameObject;
+                GameObject H = G.gameObject.GetComponent<TreasureChest>().CheckLootNearChest() as GameObject;
+                H.gameObject.GetComponent<TreasureChest>().DropLootNearChest(1);
             }
         }
 
-        if(dungeonNumber==0)
-        {
-            //dungeonAprefab.gameObject.SetActive(true);
-            //Instantiate(dungeonAprefab, new Vector2(enemyLocationX + n, enemyLocationY + n), Quaternion.identity);
-            GameObject A = dungeonAprefab.gameObject.GetComponent<TreasureChest>().CheckLootNearChest() as GameObject;
-            A.gameObject.GetComponent<TreasureChest>().DropLootNearChest(1);
-        }
 
-        if (dungeonNumber == 1)
-        {
-            //dungeonBprefab.gameObject.SetActive(true);
-            //Instantiate(dungeonBprefab, new Vector2(enemyLocationX + n, enemyLocationY + n), Quaternion.identity);
-            GameObject B = dungeonAprefab.gameObject.GetComponent<TreasureChest>().CheckLootNearChest() as GameObject;
-            B.gameObject.GetComponent<TreasureChest>().DropLootNearChest(1);
-        }
-        if (dungeonNumber == 2)
-        {
-            //dungeonCprefab.gameObject.SetActive(true);
-            //Instantiate(dungeonCprefab, new Vector2(enemyLocationX + n, enemyLocationY + n), Quaternion.identity);
-            GameObject C = dungeonAprefab.gameObject.GetComponent<TreasureChest>().CheckLootNearChest() as GameObject;
-            C.gameObject.GetComponent<TreasureChest>().DropLootNearChest(1);
-        }
-        if (dungeonNumber == 3)
-        {
-            //dungeonDprefab.gameObject.SetActive(true);
-            // Instantiate(dungeonDprefab, new Vector2(enemyLocationX + n, enemyLocationY + n), Quaternion.identity);
-            GameObject D = dungeonAprefab.gameObject.GetComponent<TreasureChest>().CheckLootNearChest() as GameObject;
-            D.gameObject.GetComponent<TreasureChest>().DropLootNearChest(1);
-        }
     }
-
-
-}
