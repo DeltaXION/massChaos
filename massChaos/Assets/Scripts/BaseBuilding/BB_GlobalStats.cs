74fbcf41306a0537a3efedd8fdc49f0643ccbbf1
Amar <amar.ravi@outlook.com>
2019-07-25 11:32:41 +0530

    Merge 5 - BB(Arjun) (#41)
    * NPC assign list and Population Setup
    
    * GlobalStats

--- massChaos/Assets/Scripts/BaseBuilding/BB_GlobalStats.cs
+++ massChaos/Assets/Scripts/BaseBuilding/BB_GlobalStats.cs
@@ -5,16 +5,31 @@ using UnityEngine;
 public class BB_GlobalStats : MonoBehaviour
 {
     public static int BasePopulation, BaseAssignedNPCs, BaseAffinityAssignedNPCs, BaseTotalBuildings, BaseBuildingDamage;
-   
+
+    [SerializeField] int numberOfHouses = 0;
+    [SerializeField] public int populationCapacity = 0;
+    BB_BuildingUpgrades bB_BuildingUpgrades;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        bB_BuildingUpgrades = FindObjectOfType<BB_BuildingUpgrades>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void addToPopulationCapacity(int count)
+    {
+        populationCapacity += count;
+    }
+
+    public void CurrentNumberOfHouses()
+    {
+        numberOfHouses++;
     }
 }
