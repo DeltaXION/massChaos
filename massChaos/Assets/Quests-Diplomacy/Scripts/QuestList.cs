187a752b75d372cb2478293ce2a3ede11a638c78
Joe Sujin <joesujin@gmail.com>
2019-07-25 23:35:54 +0530

    The final push!!!... for now.
    Joebro did it! I had my doubts about it tbh and admittedly pushed him real hard but this guy is tougher than a rhinoceros. JOBROOOOOOOOBROJO!!!!

--- massChaos/Assets/Quests-Diplomacy/Scripts/QuestList.cs
+++ massChaos/Assets/Quests-Diplomacy/Scripts/QuestList.cs
@@ -9,6 +9,7 @@ public class QuestList : MonoBehaviour
     public string QuestText, AdditionalQuestText, QuestRewardsText, ListofRewards, AddText;
 
     public GameObject FollowerSlotActive;
+    public string NpcRace;
 
 
     public int Prestige_Nomads, Prestige_Ferrarium, Prestige_Froots, Prestige_Mimax,
@@ -91,25 +92,28 @@ public class QuestList : MonoBehaviour
     public void SelectQuest(int NodeQuestNumber)
     {
         Questnumber = NodeQuestNumber;
-
+        
+        
         Prestige_Nomads = Prestige_Ferrarium = Prestige_Froots = Prestige_Mimax = LootReward_Iron = LootReward_Wood = LootReward_Food = LootReward_Gold =
                 ItemReward_Uncommon = ItemReward_Common = ItemReward_Rare = ItemReward_Recipe = ItemReward_Boss = Reward_TimeChange = Quest_Time = 0; //Basically set everything to zero
 
         AdditionalQuestText = ""; //To reset Additional Questtext
         AddText = "";
 
+        GameObject.Find("DBManager").GetComponent<dbManager>().GetQuest(NodeQuestNumber);
+        AssignQuestValues();
 
         //NOMAD...NOMAD...NOMAD...NOMAD...NOMAD...NOMAD...NOMAD...NOMAD...NOMAD...NOMAD...NOMAD...NOMAD...NOMAD...NOMAD...NOMAD...NOMAD...NOMAD...NOMAD...NOMAD...
         if (Questnumber == 1)
         {
-
-            QuestText = "The Nomads are looking for a fighter to help them with a dungeon.";
+            
+            /*QuestText = "The Nomads are looking for a fighter to help them with a dungeon.";
 
             Prestige_Nomads = 5;
-            Quest_Time = 4;
-            
-            
-            if(FollowerSlotActive.name != "DummyFollowerSlot" && FollowerSlotActive.GetComponent<FollowerSlot>().FollowerRace == "Nomad")
+            Quest_Time = 4;*/
+
+
+            if (FollowerSlotActive.name != "DummyFollowerSlot" && FollowerSlotActive.GetComponent<FollowerSlot>().FollowerRace == "Nomad")
             {
                 
                 AddText = "The Nomad valiantly springs forth from the crowd to answer the call of Cleansing. \n";
@@ -145,12 +149,6 @@ public class QuestList : MonoBehaviour
 
         if (Questnumber == 2)
         {
-            QuestText = "Iron is scarce and the Nomads can't quell evil and death without weapons of destruction. They're asking for assistance in mining.";
-            Prestige_Nomads = 5;
-            Quest_Time = 4;
-            LootReward_Iron = 5;
-
-
             if (FollowerSlotActive.name != "DummyFollowerSlot" && FollowerSlotActive.GetComponent<FollowerSlot>().FollowerRace == "Ferrarium")
             {
                 AddText = "The skilled and crafty amalgamation of iron and flesh is well experienced on how to sap the earth of its riches. \n";
@@ -174,12 +172,6 @@ public class QuestList : MonoBehaviour
 
         if (Questnumber == 3)
         {
-            QuestText = "Deep in the mountainside are great deposits of iron. Help the Nomads claim this bounty.";
-            Prestige_Nomads = 2;
-
-            Quest_Time = 4;
-            LootReward_Iron = 10;
-
             if (FollowerSlotActive.name != "DummyFollowerSlot" && FollowerSlotActive.GetComponent<FollowerSlot>().FollowerRace == "Ferrarium")
             {
                 AddText = "The skilled and crafty amalgamation of iron and flesh is well experienced on how to sap the earth of its riches. \n";
@@ -193,12 +185,7 @@ public class QuestList : MonoBehaviour
 
         if (Questnumber == 4)
         {
-            QuestText = "A curse has befallen a Nomad hut. They begrudgingly request assistance from a mage.";
-            
-            Quest_Time = 4;
-            LootReward_Gold = 5;
-
-            if (FollowerSlotActive.name != "DummyFollowerSlot" && FollowerSlotActive.GetComponent<FollowerSlot>().FollowerClass == "maige")
+           if (FollowerSlotActive.name != "DummyFollowerSlot" && FollowerSlotActive.GetComponent<FollowerSlot>().FollowerClass == "maige")
             {
                 AddText = "The incantations chanted aloud by your follower send children scurrying and peeking from meek corners out of fear; not of the unknown or harm but rather that invoked by indoctrination.";
                 Prestige_Nomads -= 2;
@@ -210,12 +197,6 @@ public class QuestList : MonoBehaviour
 
         if (Questnumber == 5)
         {
-            QuestText = "Iron mining has reduced an entire forest to dirt. The Nomads are looking for help on the matter.";
-            Prestige_Nomads = 5;
-
-            Quest_Time = 15;
-            LootReward_Gold = 10;
-
             if (FollowerSlotActive.name != "DummyFollowerSlot" && FollowerSlotActive.GetComponent<FollowerSlot>().FollowerRace == "Froots")
             {
                 AddText = "Healing takes time and more so when it is the earth in pain. But a Froot also knows how to soothe any ailing thing. \n";
@@ -229,9 +210,6 @@ public class QuestList : MonoBehaviour
 
         if (Questnumber == 6)
         {
-            QuestText = "A blooming of the IronPetals congregates the Nomads to the camp. You could send a follower to witness the event.";
-            Quest_Time = 4;
-
             if (FollowerSlotActive.name != "DummyFollowerSlot" && FollowerSlotActive.GetComponent<FollowerSlot>().FollowerRace == "Nomad")
             {
                 AddText = "Healing takes time and more so when it is the earth in pain. But a Froot also knows how to soothe any ailing thing. \n";
@@ -399,5 +377,60 @@ public class QuestList : MonoBehaviour
         }
     }
 
-    
+
+    void AssignQuestValues()
+    {
+        List<Quest> dbQuests = dbManager.quests;
+        foreach (Quest quest in dbQuests)
+        {
+            Questnumber = quest.Questnumber;
+            QuestText = quest.QuestText;
+            AdditionalQuestText = quest.AdditionalQuestText;
+            AddText = quest.AddText;
+            QuestRewardsText = quest.QuestRewardsText;
+            ListofRewards = quest.ListofRewards;
+            Prestige_Nomads = quest.Prestige_Nomads;
+            Prestige_Ferrarium = quest.Prestige_Ferrarium;
+            Prestige_Froots = quest.Prestige_Froots;
+            Prestige_Mimax = quest.Prestige_Mimax;
+            LootReward_Iron = quest.LootReward_Iron;
+            LootReward_Wood = quest.LootReward_Wood;
+            LootReward_Food = quest.LootReward_Food;
+            LootReward_Gold = quest.LootReward_Gold;
+            ItemReward_Uncommon = quest.LootReward_Gold;
+            ItemReward_Common = quest.ItemReward_Common;
+            ItemReward_Rare = quest.ItemReward_Rare;
+            ItemReward_Recipe = quest.ItemReward_Recipe;
+            ItemReward_Boss = quest.ItemReward_Boss;
+            Quest_Time = quest.Quest_Time;
+            Reward_TimeChange = quest.Reward_TimeChange;
+        }
+    }
+
+
+    /*void UpdateQuestValues(int Quest_id)
+    {
+        NpcRace = FollowerSlotActive.GetComponent<FollowerSlot>().FollowerRace;
+        List<Quest> dbQuests = dbManager.quests;
+        foreach (Quest quest in dbQuests)
+        {
+            if (1 >= quest.Questnumber && quest.Questnumber <= 6)
+            {
+                if (NpcRace == "Nomads")
+                {
+                    dbManager.quests.Clear();
+                    GameObject.Find("DBManager").GetComponent<dbManager>().UpdateQuest(Quest_id, NpcRace);
+                }
+            }
+            else if (16 >= quest.Questnumber && quest.Questnumber <= 21)
+            {
+                if (NpcRace == "Ferrarium")
+                {
+                    dbManager.quests.Clear();
+                    GameObject.Find("DBManager").GetComponent<dbManager>().UpdateQuest(Quest_id, NpcRace);
+                }
+            }
+        }
+    }*/
+
 }
---
+++ massChaos/Assets/Quests-Diplomacy/Scripts/dbManager.cs
@@ -0,0 +1,193 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.Data;
+using Mono.Data.Sqlite;
+
+public class dbManager : MonoBehaviour
+{
+    private int QuestId;
+    private bool RaceCheck;
+    private string NpcRace;
+
+    private string connectionString;
+    private IDbConnection dbConnection;
+    private IDbCommand dbCommand;
+    private IDataReader dataReader;
+
+    public GameObject FollowerSlotActive;
+
+    public static List<Quest> quests = new List<Quest>();
+
+
+    void Start()
+    {
+       
+        //QuestId = QuestList;
+        //NpcRace = FollowerSlotActive.GetComponent<FollowerSlot>().FollowerRace; //getting npc race
+        connectionString = "URI=file:" + Application.dataPath + "/Questsdb.db"; 
+        //GetQuest(QuestId);
+
+        
+        //Debug.Log(quests[0].Questnumber + "__&&__" + quests[0].QuestText);
+
+        //AssignNpc(QuestId, 1);
+
+        /*if (1 >= quests[0].Questnumber && quests[0].Questnumber <= 6)
+        {
+            UpdateQuest(quests[0].Questnumber, NpcRace);
+        }
+        else if (16 >= quests[0].Questnumber && quests[0].Questnumber <= 21)
+        {
+            UpdateQuest(quests[0].Questnumber, NpcRace);
+        }
+        Debug.Log(quests[0].Questnumber + "__&&__" + quests[0].QuestText);*/
+        //InsertData(5, 2);
+    }
+
+
+
+    void Update()
+    {
+
+        
+    }
+
+    public void getQuestnumber(int quest_number)
+    {
+        QuestId = quest_number;
+    }
+
+    
+
+    public void GetQuest(int Quest_Id)
+    {
+        quests.Clear();
+
+        using (dbConnection = new SqliteConnection(connectionString))
+        {
+            dbConnection.Open();
+            
+            using (dbCommand = dbConnection.CreateCommand())
+            {
+                string sqlQuery = String.Format("SELECT * FROM QuestList WHERE Questnumber = {0}; ", Quest_Id);
+                dbCommand.CommandText = sqlQuery;
+
+                using (dataReader = dbCommand.ExecuteReader())
+                {
+                    while (dataReader.Read())
+                    {
+                        Quest q = new Quest();
+                        Debug.Log("dog days are overrrrr");
+                        //Debug.Log(dataReader.GetString(1) + " _ " + dataReader.GetValue(3));
+                        q.Questnumber = dataReader.GetInt32(0);
+                        q.QuestText = dataReader.GetString(1);
+                        q.AdditionalQuestText = dataReader.GetString(2);
+                        q.AddText = dataReader.GetString(3);
+                        q.QuestRewardsText = dataReader.GetString(4);
+                        q.ListofRewards = dataReader.GetString(5);
+                        q.Prestige_Nomads = dataReader.GetInt32(6);
+                        q.Prestige_Ferrarium = dataReader.GetInt32(7);
+                        q.Prestige_Froots = dataReader.GetInt32(8);
+                        q.Prestige_Mimax = dataReader.GetInt32(9);
+                        q.LootReward_Iron = dataReader.GetInt32(10);
+                        q.LootReward_Wood = dataReader.GetInt32(11);
+                        q.LootReward_Food = dataReader.GetInt32(12);
+                        q.LootReward_Gold = dataReader.GetInt32(13);
+                        q.ItemReward_Uncommon = dataReader.GetInt32(14);
+                        q.ItemReward_Common = dataReader.GetInt32(15);
+                        q.ItemReward_Rare = dataReader.GetInt32(16);
+                        q.ItemReward_Recipe = dataReader.GetInt32(17);
+                        q.ItemReward_Boss = dataReader.GetInt32(18);
+                        q.Quest_Time = dataReader.GetInt32(19);
+                        q.Reward_TimeChange = dataReader.GetInt32(20);
+                        
+                        quests.Add(q);
+                        Debug.Log("dog days are overrrrr");
+                    }
+                    dbConnection.Close();
+                    dataReader.Close();
+                }
+            }
+        }
+    }
+
+    public void UpdateQuest(int Quest_Id ,string race)
+    {
+        using (dbConnection = new SqliteConnection(connectionString))
+        {
+            dbConnection.Open();
+
+            using (dbCommand = dbConnection.CreateCommand())
+            {
+                string sqlQuery = String.Format("select * from If_"+race+"_Rewardslist where Questnumber = '{0}' ", Quest_Id);
+
+                dbCommand.CommandText = sqlQuery;
+                Debug.Log("dog days are overrrrr");
+
+
+                using (dataReader = dbCommand.ExecuteReader())
+                {
+                    while (dataReader.Read())
+                    {
+                        Quest q = new Quest();
+                        //Debug.Log(dataReader.GetString(1) + " _ " + dataReader.GetValue(3));
+                        q.Questnumber = dataReader.GetInt32(0);
+                        q.QuestText = dataReader.GetString(1);
+                        q.AdditionalQuestText = dataReader.GetString(2);
+                        q.AddText = dataReader.GetString(3);
+                        q.QuestRewardsText = dataReader.GetString(4);
+                        q.ListofRewards = dataReader.GetString(5);
+                        q.Prestige_Nomads = dataReader.GetInt32(6);
+                        q.Prestige_Ferrarium = dataReader.GetInt32(7);
+                        q.Prestige_Froots = dataReader.GetInt32(8);
+                        q.Prestige_Mimax = dataReader.GetInt32(9);
+                        q.LootReward_Iron = dataReader.GetInt32(10);
+                        q.LootReward_Wood = dataReader.GetInt32(11);
+                        q.LootReward_Food = dataReader.GetInt32(12);
+                        q.LootReward_Gold = dataReader.GetInt32(13);
+                        q.ItemReward_Uncommon = dataReader.GetInt32(14);
+                        q.ItemReward_Common = dataReader.GetInt32(15);
+                        q.ItemReward_Rare = dataReader.GetInt32(16);
+                        q.ItemReward_Recipe = dataReader.GetInt32(17);
+                        q.ItemReward_Boss = dataReader.GetInt32(18);
+                        q.Quest_Time = dataReader.GetInt32(19);
+                        q.Reward_TimeChange = dataReader.GetInt32(20);
+
+                        quests.Add(q);
+                    }
+                    dbConnection.Close();
+                    dataReader.Close();
+                }
+            }
+        }
+    }
+
+
+    /*private void AssignNpc(int Quest_id, int Npc_id)
+    {
+        using (dbConnection = new SqliteConnection(connectionString))
+        {
+            dbConnection.Open();
+
+            using (dbCommand = dbConnection.CreateCommand())
+            {
+
+                //string delete_table = "delete from Npc_Check";
+
+                string AssignNpcQuest = String.Format("update Quest_List set Npc_Id = '{0}' where Quest_id = {1};", Npc_id,Quest_id);
+                
+                //dbCommand.CommandText = delete_table;
+                //dbCommand.ExecuteScalar();
+                dbCommand.CommandText = AssignNpcQuest;
+                dbCommand.ExecuteScalar();
+                dbConnection.Close();
+
+
+
+            }
+        }
+
+    }*/
+}
