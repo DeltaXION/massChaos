b8adf0aa5890868e147a429072c655cb1ca79ec6
Amar <amar.ravi@outlook.com>
2019-07-25 23:56:04 +0530

    Scene Merge v1 - Rajat needs to modify room prefabs

--- massChaos/Assets/Combat/Ranged Combat/playershoot.cs
+++ massChaos/Assets/Combat/Ranged Combat/playershoot.cs
@@ -7,7 +7,13 @@ public class playershoot : MonoBehaviour
     public float bulletVelocity = 5f;
     public GameObject bullet;
     public GameObject bullet1;
+    public GameObject bulletsp;
+    public GameObject bulletsp2;
     public float TimeToLive = 5f;
+    public PlayerState st;
+    float startTime;
+    float timer = 0.0f;
+    int weaptype;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,11 +23,34 @@ public class playershoot : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButtonDown("attack") /*&& currentState != PlayerState.attack*/)
+        //state current;
+        weaptype = gameObject.GetComponent<PlayerMovement>().pweapon;
+        st = gameObject.GetComponent<PlayerMovement>().currentState;
+        if (weaptype == 4)
         {
-            //currentState =
-            StartCoroutine(Attackarrow());
-            
+            if (Input.GetButtonDown("attack") /*&& currentState != PlayerState.attack*/)
+            {
+                //currentState =
+                timer += Time.fixedDeltaTime;
+                startTime = Time.time;
+                st = PlayerState.walkslow;
+                // StartCoroutine(Attackarrow());
+
+            }
+            if (Input.GetButtonUp("attack"))
+
+            {
+                // Debug.Log("release");
+                // if (timer >= 0.1f)
+                if (Time.time - startTime >= 0.1f)
+                {
+                    StartCoroutine(Attackarrow2());
+                }
+                else
+                    StartCoroutine(Attackarrow());
+                st = PlayerState.walk;
+
+            }
         }
     }
 
@@ -45,5 +74,26 @@ public class playershoot : MonoBehaviour
         
         // Adds velocity to the bullet
         bullet.GetComponent<Rigidbody2D>().velocity = direction * bulletVelocity;
+    }
+    private IEnumerator Attackarrow2()
+    {
+        // animator.SetBool("attacking", true);
+        // currentState = PlayerState.attack;
+        yield return null;
+        //Debug.Log(myRigidbody.velocity);
+        // animator.SetBool("attacking", false);
+        yield return new WaitForSeconds(0.2f);
+        // currentState = PlayerState.walk;
+        Vector3 worldMousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 direction = (Vector2)((worldMousePos - transform.position));
+        direction.Normalize();
+        // Creates the bullet locally
+        GameObject bulletsp = (GameObject)Instantiate(
+                                bulletsp2,
+                                transform.position + (Vector3)(direction * 0.5f),
+                                Quaternion.identity);
+
+        // Adds velocity to the bullet
+        bulletsp.GetComponent<Rigidbody2D>().velocity = direction * bulletVelocity;
     }
 }
