c6499175795fe4a3f619ef86d911b717cef8ee91
taastingo <44228598+taastingo@users.noreply.github.com>
2019-07-26 00:02:11 +0530

    Farm Final
    Sensei High Quality Agri-Growers

---
+++ massChaos/Assets/farming/Plants/PlantSprites/ClickToSow.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickToSow : MonoBehaviour
+
+{
+    Timer2 timer;
+    public GameObject apple;
+    public GameObject parentList;
+    public float plantGrowDuration;
+    static float harvestIn;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        harvestIn += Time.deltaTime;
+        
+    }
+
+    public void AddApple()
+    {
+        var a = Instantiate(apple);
+        Debug.Log(harvestIn);
+        a.transform.SetParent(parentList.transform);
+        a.transform.localScale = new Vector3(1, 1, 0);
+        Debug.Log(a.tag);
+        
+    }
+}
