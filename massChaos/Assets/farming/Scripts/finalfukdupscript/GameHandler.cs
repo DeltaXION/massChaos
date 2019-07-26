95f794215bcfa4103f792485002d571698917a9b
pralay_m <pralay_m@nid.edu>
2019-07-26 07:45:12 +0530

    added a very jugadu add follower menu... applying follower allows to plant a seed. Time is ticking but only in Console...

---
+++ massChaos/Assets/farming/Scripts/finalfukdupscript/GameHandler.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class GameHandler : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void Start()
+    {
+        //new IdontKnowDaFaqImDoing(Vector3.zero); 
+        TickTimerScript.OnTick += delegate (object sender, TickTimerScript.onTickEventArgs e)
+          {
+              Debug.Log("tick" + e.tick);
+          };
+    }
+
+    // Update is called once per frame
+  private  void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            //new IdontKnowDaFaqImDoing (50);
+        }
+    }
+}
---
+++ massChaos/Assets/farming/Scripts/finalfukdupscript/TickTimerScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using CollabProxy;
+
+public class TickTimerScript : MonoBehaviour
+{
+    public class onTickEventArgs : EventArgs
+    {
+        public int tick;
+    }
+    public static event EventHandler<onTickEventArgs> OnTick;
+    private const float TICK_TIMER_MAX = .2f;
+    private int tick;
+    private float tickTimer;
+
+
+    private void Awake()
+    {
+        tick = 0;
+    }
+
+    private void Update()
+    {
+        tickTimer += Time.deltaTime;
+        if (tickTimer>=TICK_TIMER_MAX)
+        {
+            tickTimer -= TICK_TIMER_MAX;
+            tick++;
+            if (OnTick != null) OnTick(this,new onTickEventArgs { tick = tick });
+            Debug.Log("tick" + tick);
+            
+        }
+    }
+}
